d5731bae3d3ffe0c8c1edb1b6340f622ce570ed2
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-08 14:50:30 -0700

    Add autction item bids

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,20 @@
 class Item
-    attr_reader :name
+    attr_reader :name, :bids
+
     def initialize(name)
         @name = name
+        @bids = {}
+    end
+
+    def add_bid(attendee, amount)
+        @bids[attendee] = amount
+    end
+
+    def current_high_bid
+        highest_bid = []
+        @bids.each do |attendee, bid|
+            highest_bid << bid
+        end
+        highest_bid.max
     end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Auction do
     let(:auction) { Auction.new }
     let(:item1) { Item.new('Chalkware Piggy Bank') }
     let(:item2) { Item.new('Bamboo Picture Frame') }
+    let(:item3) { Item.new('Homemade Chocolate Chip Cookies') }
+    let(:item4) { Item.new('2 Days Dogsitting') }
+    let(:item5) { Item.new('Forever Stamps') }
+    let(:attendee1) { Attendee.new(name: 'Megan', budget: '$50') }
+    let(:attendee2) { Attendee.new(name: 'Bob', budget: '$75') }
+    let(:attendee3) { Attendee.new(name: 'Mike', budget: '$100') }
 
     describe "#initialize" do
         it "exists" do
@@ -24,6 +30,25 @@ RSpec.describe Auction do
 
             expect(auction.items).to eq([item1, item2])
             expect(auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+        end
+    end
+
+    describe "item bids" do
+        it "starts with no bids" do
+            expect(item1.bids).to eq({})
+        end
+        
+        it "can add bids and have a current high bid" do
+            item1.add_bid(attendee2, 20)
+            item1.add_bid(attendee1, 22)
+        
+            bids_by_attendees = {
+                attendee1 => 22,
+                attendee2 => 20
+            }
+        
+            expect(item1.bids).to eq(bids_by_attendees)
+            expect(item1.current_high_bid).to eq(22)
         end
     end
 end
