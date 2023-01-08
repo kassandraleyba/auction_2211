77c1f48120f677f3abdae6ffc2b938ab791cc2bb
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-08 14:41:15 -0700

    Complete iteration_1

--- lib/auction.rb
+++ lib/auction.rb
@@ -8,4 +8,12 @@ class Auction
     def add_item(item)
         @items << item
     end
+
+   def item_names
+    item_names = []
+        @items.each do |item|
+            item_names << item.name
+        end
+        item_names
+    end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe Auction do
             auction.add_item(item2)
 
             expect(auction.items).to eq([item1, item2])
+            expect(auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
         end
     end
 end
